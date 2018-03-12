a652a91c6c0339528543d1cbe4b3650c2308b60b
Shaun Tung <shaun.tung@gmail.com>
2018-03-12 19:00:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function removeFromCart(name) {
 
 
 function placeOrder(ccn) {
-  if (!(ccn === int)){
+  if (!(ccn = int)){
     return `Sorry, we don't have a credit card on file for you.`
   }
   
