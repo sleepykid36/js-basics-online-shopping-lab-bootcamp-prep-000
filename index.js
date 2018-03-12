34defb1fcaab81ea1a9395f77fa931a533d1b0b4
Shaun Tung <shaun.tung@gmail.com>
2018-03-12 19:04:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function placeOrder(ccn) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   else {
-    return `Your total is $${totalInCart}
+    return `Your total is $${totalInCart}`
     cart = []
     return cart
   }
